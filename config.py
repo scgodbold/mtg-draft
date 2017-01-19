717ea2825a255ddfa7bc53d4ce312223a7239be0
Scott Godbold <scgodbold@gmail.com>
2017-01-18 21:17:10 -0500

    Typo Fix & gitignore update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEBUG = secret.DEBUG
 SECRET_KEY = secret.SECRET_KEY
 
 # SQL settings
-SQLALCHEMT_DATABASE_URI = secret.SQLALCHEMY_DATABASE_URI
+SQLALCHEMY_DATABASE_URI = secret.SQLALCHEMY_DATABASE_URI
 
 # Use for random name generation
 ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
